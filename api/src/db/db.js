a32daef6cf52a939a792e32341a9bdd678f37122
Charbel-Tannous <tannous.j.charbel@gmail.com>
2024-02-02 23:46:06 +0200

    optimizing API & update todo

--- api/src/db/db.js
+++ api/src/db/db.js
@@ -1,6 +1,14 @@
 const config = require("./config");
 const sql = require("mssql");
 
+async function getConnection(){
+    try{
+        const pool = await sql.connect(config);
+        return pool;
+    }catch(e){
+        console.error('SQL Connection Error: ', error);
+    }
+}
 const db = new sql.ConnectionPool(config);
 
-module.exports = db;
+module.exports = {getConnection}
--- app/src/components/Todo.jsx
+++ app/src/components/Todo.jsx
@@ -1,44 +1,63 @@
 import "./Todo.css";
 import Modal from "./Modal";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Todo(props) {
   const [open, setOpen] = useState(false);
-  const openModal = () =>{
+  const navigate = useNavigate();
+  const openModal = () => {
     setOpen(true);
-  }
-  const closeModal = () =>{
+  };
+  const closeModal = () => {
     setOpen(false);
-  }
+  };
+
+  const editTodo = () => {
+    navigate({ pathname: "/newTodo", search: `?type=edit&id=${props.todo.todo_id}` });
+  };
 
   const deleteTodo = async () => {
-    const response = await fetch(`http://localhost:3000/api/todo?todoId=${props.todo.todo_id}`, {
-      method: 'DELETE',
-      headers: {
-        "Content-Type": "application/json",
+    const response = await fetch(
+      `http://localhost:3000/api/todo?todoId=${props.todo.todo_id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    });
+    );
     const data = await response.json();
     console.log(data);
-    if(data.success){
-      alert('deleted successfully.');
-    }else{
+    if (data.success) {
+      alert("deleted successfully.");
+    } else {
       alert(data.message);
     }
-  }
+  };
 
   const confirmDelete = () => {
     deleteTodo();
     closeModal();
-  }
+  };
 
   return (
     <>
       <article>
-        <header>{props.todo.todo_title}</header>
+        <header className="title">
+          <h2>{props.todo.todo_title}</h2>
+          <button className="outline" onClick={editTodo}>Edit</button>
+        </header>
         <p>{props.todo.todo_desc}</p>
         <div className="options">
-          <button className="outline" onClick={openModal} data-tooltip="Delete Todo" data-placement="bottom">Delete</button>
+          <button
+            className="outline"
+            onClick={openModal}
+            data-tooltip="Delete Todo"
+            data-placement="bottom"
+          >
+            Delete
+          </button>
         </div>
       </article>
 
